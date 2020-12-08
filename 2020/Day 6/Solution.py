e3c351fdb5ed8eba3080841f26daef6b237424a0
defracted <72914371+defracted@users.noreply.github.com>
2020-12-08 20:54:56 +0400

    Finally foudn a way to solve Day 6 Part 2

--- 2020/Day 6/Solution.py
+++ 2020/Day 6/Solution.py
@@ -1,13 +1,20 @@
 import os
-from typing import Counter
 
 with open(os.path.join(os.path.dirname(__file__), "input.txt")) as fp:
     raw_data = fp.read()
 
 groups = raw_data.split("\n\n")
-groups_straightened = [set(group.replace("\n", "")) for group in groups]
+groups = [set(group.replace("\n", "")) for group in groups]
 
-groups_calculated = [len(group) for group in groups_straightened]
+groups_calculated = [len(group) for group in groups]
 result1 = sum(groups_calculated)
 
-print(f"part 1 — {result1}")
+print(f"Part 1 — {result1}")
+
+groups = [[set(answers) for answers in group.split("\n")] for group in raw_data.split("\n\n")]
+
+result2 = 0
+for group in groups:
+    result2 += len(group[0].intersection(*group[1:]))
+
+print(f"Part 2 — {result2}")
