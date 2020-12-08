1dea2743d373064c8295296d9e24797c72db6e09
defracted <72914371+defracted@users.noreply.github.com>
2020-12-08 16:50:11 +0400

    Kinda formatted the code

--- 2020/Day 5/Solution.py
+++ 2020/Day 5/Solution.py
@@ -5,8 +5,9 @@ import os.path
 with open(os.path.join(__file__, "../input.txt"), mode="r", encoding="utf-8") as fp:
     data = [line.removesuffix("\n") for line in fp.readlines()]
 
+
 def get_seat_id(seat):
-    transformation = { 'F': '0', 'B': '1', 'L': '0', 'R': '1'}
+    transformation = {"F": "0", "B": "1", "L": "0", "R": "1"}
     binary = "".join([transformation[x] for x in seat])
     return int(binary, 2)
 
@@ -18,5 +19,5 @@ print(f"Part 1 — {max(ids)}")
 for i in range(max(ids)):
     low = i - 1
     high = i + 1
-    if  low in ids and high in ids and i not in ids:
+    if low in ids and high in ids and i not in ids:
         print(f"Part 2 — {i}")
